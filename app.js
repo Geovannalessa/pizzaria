5076d29ccc9f82cb1c5675419a15fad7cba710dd
Geovanna Lessa <geovannalessa2@gmail.com>
2022-03-04 17:45:19 -0300

    Fase 4 semi finalizada

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+
 const { listen } = require('express/lib/application');
 const app = express();
-
+const PizzasRouter = require('./router/PizzasRouter')
+app.use('/', PizzasRouter.js)
 app.get('/', (req, res) => {
     console.log('raiz rodando ºuº.. na porta 3000')
     res.send('Olá visitante')
